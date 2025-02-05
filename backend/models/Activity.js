63604b6c23a5cee211d7070005afa57fb36e2658
Lalit <lalitjindal0295@gmail.com>
2025-02-05 21:02:24 +0530

    commit

--- backend/models/Activity.js
+++ backend/models/Activity.js
@@ -1,5 +1,38 @@
-"activity"
-import mongoose from mongoose;
-import { Schema } from 'mongoose';
+import mongoose,{ Schema } from mongoose;
 
-const activitySchema=mongoose.Schema
+const activitySchema=mongoose.Schema({
+        activityId:{
+            type:mongoose.Schema.type.objectId,
+            required:true,
+            default: () => new mongoose.schema.Types.ObjectId()
+        },
+        name:{
+            type:String,
+            required:true,
+        },
+        typeOfActivity:{
+            type:String,
+            required:true,
+        },
+        description:{
+            type:String,
+            required:true,
+        },
+        instructions:{
+            type:String,
+            required:true,
+        },
+        resources:[{
+            typeOfResources:{
+                type:String,
+                required:true,
+            },
+            url:{
+                type:String,
+                required:true,
+            }
+        }]
+})
+
+const activity=mongoose.model("activity",activitySchema);
+export default activity;
