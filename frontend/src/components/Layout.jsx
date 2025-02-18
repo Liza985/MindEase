bbaa37bb459359cdace8c54de72b47d54154ae15
Khushi Agarwal <agrwlkhushi27@gmail.com>
2025-02-18 20:54:04 +0530

    auth

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+const Layout = ({ children }) => {
+	return (
+		<div>
+			<Header />
+			<main style={{ minHeight: "70vh" }}>{children}</main>
+		</div>
+	);
+};
+
+export default Layout;
