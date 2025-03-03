d0ea4f2c55880ade423cc831eeb07c876105aa87
Khushi Agarwal <agrwlkhushi27@gmail.com>
2025-03-03 23:51:37 +0530

    commit

--- frontend/src/routes/user/User.jsx
+++ frontend/src/routes/user/User.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Auth from "../../pages/Auth";
 import Blogs from "../../pages/user/Blogs";
@@ -6,6 +5,8 @@ import CounselingPage from "../../pages/user/Counsellors";
 import Home from "../../pages/user/Home";
 import HowItWorks from "../../pages/user/HowItWorks";
 import WellnessHub from "../../pages/user/WellnesaHub";
+import VerifyOtp from "../../pages/VerifyOtp";
+import ForgotPassword from "../../pages/ForgotPassword";
 
 const User = () => {
 	return (
@@ -27,6 +28,26 @@ const User = () => {
 						<Auth />
 						//</AuthRoute>
 					}
+				/>
+				<Route
+					path="/verify/:id"
+					element={
+						//<AuthRoute>
+						<VerifyOtp />
+						//</AuthRoute>
+					}
+				/>
+				<Route
+					path="/forgot-password"
+					element={<ForgotPassword />}
+				/>
+				<Route
+					path="/forgot-password/:id"
+					element={<ForgotPassword />}
+				/>
+				<Route
+					path="/changepassword/:id"
+					element={<ForgotPassword />}
 				/>
 				<Route path="/" element={<Home />} />
 				<Route
